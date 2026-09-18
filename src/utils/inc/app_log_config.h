0a884694ef573f6e0bf03c8e5d5435cd36c3aea2
A. Elmaghrabi <a.e.maghrabi@outlook.com>
2026-09-19 01:40:38 +0300

    Switch default log level to Info

--- src/utils/inc/app_log_config.h
+++ src/utils/inc/app_log_config.h
@@ -13,7 +13,7 @@
 /// Compile-time minimum level. Log calls below it are removed from the build.
 /// One of APP_LOG_LEVEL_VERBOSE, _INFO, _WARNING, _ERROR, _NONE (no output).
 #ifndef APP_LOG_LEVEL_MIN
-#define APP_LOG_LEVEL_MIN     APP_LOG_LEVEL_VERBOSE
+#define APP_LOG_LEVEL_MIN     APP_LOG_LEVEL_INFO
 #endif
 
 /// 1: colour each line by level with ANSI escape codes. 0: plain text.
